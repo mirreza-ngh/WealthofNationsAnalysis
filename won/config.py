d04e20e19fd5d8d5b06d1912ba71d195676606e1
mirreza-ngh <reza76229@gmail.com>
2025-11-16 12:07:27 +0100

    Update config.py

--- won/config.py
+++ won/config.py
@@ -1,8 +1,8 @@
 # World Bank indicator codes and constants
-GDP_PC = "NY.GDP.PCAP.CD"        # GDP per capita (current US$)
+GDP_PC = "NY.GDP.PCAP.CD"        # GDP per capita (current dollars)
 LIFE_EXP = "SP.DYN.LE00.IN"      # Life expectancy at birth (years)
-HEALTH_SP = "SH.XPD.CHEX.PC.CD"  # Health expenditure per capita (US$)
-CHILD_MORT = "SH.DYN.MORT"       # Under-5 mortality (per 1,000 births)
+HEALTH_SP = "SH.XPD.CHEX.PC.CD"  # Health expenditure per capita (dollars)
+CHILD_MORT = "SH.DYN.MORT"       # Under 5 mortality (per 1,000 births)
 
 DEFAULT_INDICATORS = {
     "gdp_pc": GDP_PC,
